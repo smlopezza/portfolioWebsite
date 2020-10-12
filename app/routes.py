12e2a7bd01db20155a9369337f1f8c784119e09d
Sandra Milena <8998230+smlopezza@users.noreply.github.com>
2020-10-12 17:32:49 -0400

    Summary of projects was added

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 
@@ -5,6 +6,9 @@ from flask_bootstrap import Bootstrap
 
 # Redirect to "next" page
 from werkzeug.urls import url_parse
+
+# files
+from flask import send_file, send_from_directory, safe_join, abort
 
 
 @app.route('/')
@@ -23,3 +27,24 @@ def playground():
 @app.route('/aboutMe')
 def aboutMe():
     return render_template("aboutMe.html")
+
+
+# Portfolio routes
+@app.route('/portfolio_Smartsito')
+def portfolio_Smartsito():
+    return render_template("portfolio_Smartsito.html")
+
+@app.route('/portfolio_Upskilling')
+def portfolio_Upskilling():
+    return render_template("portfolio_Upskilling.html")
+
+@app.route('/portfolio_RecruitApp')
+def portfolio_RecruitApp():
+    return render_template("portfolio_RecruitApp.html")
+
+
+@app.route('/portfolioFile/<pdf_id>', methods=['GET', 'POST'])
+def portfolioFile(pdf_id):
+    filename = f"{pdf_id}.pdf"
+    uploads = os.path.join( app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(directory=uploads, filename=filename)
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PersonalWebsite'
+    UPLOAD_FOLDER = 'static/documents'
+    #UPLOAD_FOLDER = '/home/slopezza/LangsFiles'
